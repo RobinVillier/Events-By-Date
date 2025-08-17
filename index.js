3f46633f8b98bf871ad4f76babe97a17bf0eb2ff
Robin Villier <rvillier99@gmail.com>
2025-08-17 10:51:42 +0200

    Fetch images and display them on each card

--- index.js
+++ index.js
@@ -25,25 +25,49 @@ app.post("/submit", async (req, res) => {
 
   try {
     const result = await axios.get(`${API_URL}/${month}/${day}/${action}.json`);
-    const data = result["data"][action].reverse()
+    const data = result["data"][action].reverse().slice(0, 10)
+    
+    for (let index = 0; index < data.length; index++) {
+      const wiki_title = data[index].wikipedia[0].title
+      const wiki_info = await getWikipediaInfo(wiki_title);
+      wiki_info.url = data[index].wikipedia[0].wikipedia
+      data[index].wiki_info = wiki_info;
+    }
+    
+    res.render("index.ejs", { 
+      selectedAction: action,
+      data: data,
+      date: formatted_date
+    });
 
-    res.render("index.ejs", { selectedAction: action, data: data, date: formatted_date});
   } catch (error) {
     console.error(error);
     res.render("index.ejs", { selectedAction: null });
   }
 });
 
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
 
+// Custom Functions
 function getDate (day, month) { 
     const monthNames = [
         "January", "February", "March", "April", "May", "June",
         "July", "August", "September", "October", "November", "December"];
 
     return `${monthNames[month - 1]}, ${day}`;
+}
+
+async function getWikipediaInfo(title, lang = "en") {
+    const url = `https://${lang}.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(title)}`;
+    const response = await fetch(url);
+    if (!response.ok) return null;
+    const data = await response.json();
+    
+    return {
+      title: data.title || null,
+      thumbnail: data.thumbnail ? data.thumbnail.source : null
+    };
 }
